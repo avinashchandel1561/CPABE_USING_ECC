a1d6f3d3c4242e5dc7ecb70c695f2d83b63090dc
avinashchandel1561 <avinashchandel1561@gmail.com>
2022-09-28 19:18:01 +0530

    Update AuthorityInitialization.java

--- AuthorityInitialization.java
+++ AuthorityInitialization.java
@@ -1,4 +1,23 @@
+import java.util.ArrayList;
+import java.util.HashMap;
 
 public class AuthorityInitialization {
-
+	private ArrayList<Object>PP=new ArrayList<>();
+	private HashMap<Integer,HashMap<Integer,ArrayList<String>>>AA=new HashMap<>();
+	
+	AuthorityInitialization(ArrayList<Object>PP){
+		this.PP=PP;
+		
+	}
+	void SetAttributes(int authId,int userId,ArrayList<String>attributes) {
+		if(AA.containsKey(authId)) {
+			AA.get(authId).put(userId, attributes);
+		}
+		else {
+			HashMap<Integer,ArrayList<String>> temp=new HashMap<>();
+			temp.put(userId,attributes);
+			AA.put(authId,temp);
+		}
+	}
+	
 }
