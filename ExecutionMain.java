b09c1f87bb23caa19499ea024470112b316390fb
avinashchandel1561 <avinashchandel1561@gmail.com>
2022-10-01 15:53:46 +0530

    Update ExecutionMain.java
    01-10

--- ExecutionMain.java
+++ ExecutionMain.java
@@ -10,9 +10,26 @@ public class ExecutionMain {
 		sc.HashUser(101);
 		sc.setPP();
 		ArrayList<Object>PP=sc.getPP();
-		for(Object c:PP) {
-			System.out.println(c);
-		}
+//		for(Object c:PP) {
+//			System.out.println(c);
+//		}
 		//****************** GLOBAL INITIALIZATION PHASE COMPLETED ********************
+		
+		//****************** AUTHORITY INITIALIZATION PHASE STARTED ********************
+		AuthorityInitialization ai=new AuthorityInitialization(PP);
+		ai.setAttributes(1, 101, attr);
+		ai.generateKeys(1);
+//		ai.printKeys();
+		//****************** AUTHORITY INITIALIZATION PHASE COMPLETED ********************
+		
+		//****************** DATA ENCRYPTION PHASE STARTED ********************
+		
+		String MESSAGE="AVINASH CHANDEL WELCOME TO NIT ROURKELA ";
+		DataEncryption de=new DataEncryption(PP);
+		de.EncryptMessage(MESSAGE);
+		System.out.println("ENCRYPTED DATA USING AES : "+de.getCTDATA());
+		de.createHCT();
+		System.out.println(" HCDATA : "+de.getHCTDATA());
+		System.out.println(" HCDATA : "+de.getHCT().x + " "+de.getHCT().y);
 	}
 }
